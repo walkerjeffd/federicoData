e1507babf9196a51c8ed894c97b36991c2667aec
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-19 11:37:50 -0500

    add add_one() function with test and doc

---
+++ R/add_one.R
@@ -0,0 +1,12 @@
+#' Add one to the argument
+#'
+#' @param x number
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' add_one(1)
+add_one <- function(x) {
+  x + 1
+}
---
+++ tests/testthat/test-add_one.R
@@ -0,0 +1,3 @@
+test_that("add_one() adds one to the argument", {
+  expect_equal(add_one(1), 2)
+})

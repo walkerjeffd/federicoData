a89e0e9c954f55d12ae352cb313582d14dad4333
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-22 16:39:41 -0500

    add dbhydro_wq_params dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' DBHYDRO WQ Parameters.
+#'
+#' Lookup table for translating DBHYDRO \code{test_name} to
+#' \code{wq_param}, which is more concise.
+#'
+#' @format A tibble with two variables: \code{test_name} and
+#'   \code{wq_param}
+"dbhydro_wq_params"
---
+++ data-raw/dbhydro_wq_params.R
@@ -0,0 +1,8 @@
+library(readr)
+library(usethis)
+
+dbhydro_wq_params <- read_csv("data-raw/dbhydro_wq_params.csv", col_types = cols(
+  .default = col_character()
+))
+
+usethis::use_data(dbhydro_wq_params)

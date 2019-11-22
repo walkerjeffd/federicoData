8970d29c70e9f8e09c70efee005ef393989e1596
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-22 14:39:03 -0500

    add dbhydro_get_dbkey_metadata()

---
+++ R/dbhydro_get_dbkey_metadata.R
@@ -0,0 +1,58 @@
+#' Get DBKEY metadata from DBHYDRO
+#'
+#' @param dbkeys character vector of DBKEYs
+#' @param query list containing additional query parameters to pass with request
+#' @param batch_size number of DBKEYs per batch (API limit)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' dbhydro_get_dbkey_metadata(dbkeys = c("KQ646"))
+#' dbhydro_get_dbkey_metadata(dbkeys = c("KQ646", "HD889"))
+#' }
+#' @importFrom dplyr %>%
+dbhydro_get_dbkey_metadata <- function (dbkeys, query = list(), batch_size = 100) {
+  logger::log_info("fetching metadata from dbhydro for {length(dbkeys)} dbkey(s)")
+
+  stopifnot(all(!is.na(dbkeys)))
+  stopifnot(all(!duplicated(dbkeys)))
+
+  batch_dbkeys <- dbkeys[1:min(length(dbkeys), batch_size)]
+
+  logger::log_debug("dbhydro_get_dbkey_metadata: fetching {length(batch_dbkeys)} dbkeys of {length(dbkeys)} total)")
+
+  fetch_query <- c(query, list(v_dbkey = stringr::str_c(batch_dbkeys, collapse = "/")))
+  response <- httr::GET(
+    "https://my.sfwmd.gov/dbhydroplsql/show_dbkey_info.show_dbkeys_matched",
+    query = fetch_query
+  )
+  content <- httr::content(response, "parsed")
+  nodes <- rvest::html_node(content, xpath = ".//form[@name='dbkeyList']/table")
+  tbl <- rvest::html_table(nodes) %>%
+    janitor::clean_names()
+  df <- tbl %>%
+    tibble::as_tibble() %>%
+    dplyr::rename_(
+      station_id = ~station,
+      site_group = ~group
+    ) %>%
+    dplyr::select_(~-get_data) %>%
+    dplyr::mutate_(
+      start_date = ~lubridate::dmy(start_date),
+      end_date = ~lubridate::dmy(end_date),
+      latitude = ~dms_to_ddeg(latitude),
+      longitude = ~-dms_to_ddeg(longitude)
+    )
+
+  if (length(dbkeys) <= batch_size) {
+    return(df)
+  }
+
+  Sys.sleep(2)
+  dplyr::bind_rows(
+    df,
+    dbhydro_get_dbkey_metadata(dbkeys[(batch_size + 1):length(dbkeys)], query = query)
+  )
+}

8b89afab3beb019bf2101d0969945fc9e04e1176
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-22 20:33:15 -0500

    add db_connect()

---
+++ R/db_connect.R
@@ -0,0 +1,31 @@
+#' Connect to database
+#'
+#' @param host hostname
+#' @param dbname database name
+#' @param user username
+#' @param password password (optional if .pgpass is configured)
+#' @param port port (default 5432)
+#'
+#' @return DBI connection object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' con <- db_connect("localhost", "mydb", "joe", "mypassword")
+#' }
+db_connect <- function(host, dbname, user, password = NULL, port = 5432) {
+  logger::log_info("connecting to postgresql://{user}@{host}:{port}/{dbname}")
+
+  con <- DBI::dbConnect(
+    RPostgreSQL::PostgreSQL(),
+    host = host,
+    port = port,
+    user = user,
+    password = password,
+    dbname = dbname
+  )
+
+  logger::log_debug("successfully connected")
+
+  con
+}

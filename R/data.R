07010c3efc8c35aad5c658a8d88dcc8c2b04ecaa
Jeff Walker <walker.jeff.d@gmail.com>
2019-11-22 20:33:02 -0500

    move dbhydro_wq_params to internal data

--- R/data.R
+++
@@ -1,8 +0,0 @@
-#' DBHYDRO WQ Parameters.
-#'
-#' Lookup table for translating DBHYDRO \code{test_name} to
-#' \code{wq_param}, which is more concise.
-#'
-#' @format A tibble with two variables: \code{test_name} and
-#'   \code{wq_param}
-"dbhydro_wq_params"
--- data-raw/dbhydro_wq_params.R
+++ data-raw/dbhydro_wq_params.R
@@ -5,4 +5,4 @@ dbhydro_wq_params <- read_csv("data-raw/dbhydro_wq_params.csv", col_types = cols
   .default = col_character()
 ))
 
-usethis::use_data(dbhydro_wq_params)
+usethis::use_data(dbhydro_wq_params, internal = TRUE)

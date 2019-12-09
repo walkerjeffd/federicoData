bc9f28ea607ae8ba500bb2736559f6a1cc0c0bc1
Jeff Walker <walker.jeff.d@gmail.com>
2019-12-09 11:05:33 -0500

    add tracker_data()

---
+++ R/tracker_data.R
@@ -0,0 +1,39 @@
+#' Fetch tracker data from database
+#'
+#' Retrieves all hydrologic and water quality
+#' data for specified tracker and period
+#'
+#' @param con database connection object
+#' @param id tracker ID
+#' @param date_min start date
+#' @param date_max end date
+#'
+#' @return list with elements named \code{tracker}, \code{hydro}, and \code{wq}
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' tracker_data(con, id = "test-tracker") # all data
+#' tracker_data(
+#'   con,
+#'   id = "test-tracker",
+#'   date_min = "2018-10-01",
+#'   date_max = "2019-09-30"
+#' ) # specific period
+#' }
+tracker_data <- function(con, id, date_min = NULL, date_max = NULL) {
+  logger::log_info("Fetching data for tracker ({id}) from {ifelse(is.null(date_min), 'N/A', date_min)} to {ifelse(is.null(date_max), 'N/A', date_max)}")
+
+  tracker <- tracker_get(con, ids = id) %>%
+    purrr::transpose() %>%
+    purrr::pluck(1)
+  df_hydro <- NULL
+  df_wq <- NULL
+  if (!is.null(tracker$hydro)) {
+    df_hydro <- db_get_dbhydro_hydro(con, dbkeys = tracker$hydro$dbkey)
+  }
+  if (!is.null(tracker$wq)) {
+    df_wq <- db_get_dbhydro_wq(con, station_ids = tracker$wq$station_id)
+  }
+
+}
